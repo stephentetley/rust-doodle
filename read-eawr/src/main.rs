0c96545c28014dfe05811d4ac6d6b4998cc36527
stephentetley <stephen.tetley@gmail.com>
2026-06-28 14:46:48 +0100

    read-eawr - outputting Json to file

--- read-eawr/src/main.rs
+++ read-eawr/src/main.rs
@@ -13,27 +13,38 @@
 //    limitations under the License.
 
 extern crate umya_spreadsheet;
+use std::io::{BufWriter, Write};
 use std::path::{Path, PathBuf};
 use clap::Parser;
+use serde::ser::{SerializeSeq, Serializer};
 mod circuit;
 mod general_checklist;
 use circuit::{is_test_sheet, is_version_zero, read_circuit};
 use general_checklist::{get_checklist_data};
+// use serde_json::{Serializer};
 
 // Note umya-spreadsheet rather than calamine as we want random access
 
 fn read_eawr(path: &Path) {
     println!("{:?}", path);
     let workbook = umya_spreadsheet::reader::xlsx::read(path).unwrap();
-    let date_string = get_checklist_data(&workbook);
-    for i in 0..(workbook.sheet_count() - 1) {
-        let sheet = workbook.sheet(i).unwrap();
-        if is_test_sheet(sheet) && ! is_version_zero(sheet) {
-            let file_name = path.file_name().and_then(|ss| ss.to_str()).unwrap_or("");
-            let json = read_circuit(file_name, &date_string.as_str(), sheet);
-            println!("{}", json);
+    let out = std::fs::File::create("out.txt").unwrap(); 
+    let mut writer = BufWriter::new(out);
+    let mut serializer = serde_json::Serializer::new(&mut writer);
+    if let Ok(mut seq) = serializer.serialize_seq(None) {
+
+        let date_string = get_checklist_data(&workbook);
+        for i in 0..(workbook.sheet_count() - 1) {
+            let sheet = workbook.sheet(i).unwrap();
+            if is_test_sheet(sheet) && ! is_version_zero(sheet) {
+                let file_name = path.file_name().and_then(|ss| ss.to_str()).unwrap_or("");
+                let json = read_circuit(file_name, &date_string.as_str(), sheet);
+                seq.serialize_element(&json).unwrap();
+            }
         }
+        seq.end().unwrap();
     }
+    writer.flush().unwrap();
 }
 
 #[derive(Parser)]
